aca12e9150bc448a5fffc7837dde3b27751682cb
Daniil Borovkov <daniilborowkow@gmail.com>
2019-10-23 10:56:35 +0600

    WIP creating base structure for fre course page

---
+++ page-free.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Template Name: Free course
+ */
+
+get_header();
+?>
+
+<div class="genplan">
+  <div class="container-fluid author section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-7">
+          <h1>БЕСПЛАТНЫЙ БАЗОВЫЙ <br> КУРС AUTODESK CIVIL 3D</h1>
+          <p class="course-description">7 основных разделов, которые должен знать каждый инженер работающий в Civil3D</p>
+          <a href="#" class="btn btn-checkout">ПОЛУЧИТЬ КУРС</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end about author -->
+
+  <!-- course review -->
+  <div class="course-review section container">
+    <h2>ЧТО ВЫ УЗНАЕТЕ НА КУРСЕ?</h2>
+    <p>Курс, который был разработан <a href="httpss://infrabim.pro/рогачёв-игорь-витальевич/">Игорем Рогачёвым</a> и стал уже классическим. По нему выучилось не одно поколение инженеров. Курс стал очень популярным и своего рода стандартом для широкого круга инженеров применяющих Civil 3D в своей практике. Безусловно именно с него надо начинать изучение Civil 3D, тем более он бесплатный!</p>
+
+    <details class="course-contents">
+      <summary>Содержание курса</summary>
+    </details>
+  </div>
+  <!-- end course review -->
+
+  <!-- reviews -->
+  <div class="container reviews">
+    <h2 class="text-center">ОТЗЫВЫ О КУРСЕ</h2>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/css/reviews.png" alt="" class="img-responive">
+  </div>
+  <!-- end reviews -->
+
+  a.btn.btn-checkout
+</div>
+
+<?php
+get_footer();
